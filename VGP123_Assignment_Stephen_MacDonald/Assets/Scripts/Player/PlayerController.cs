1474c05f7113723d6ac3152e5d0bd997ab0a4204
BaronFangs <161364331+BaronFangs@users.noreply.github.com>
2024-11-01 23:40:20 -0400

    Updated Player Controller 1
    - Added jump
    - Added walk
    - Added animations
    - Added 1 way colliders
    - Added ground check

--- VGP123_Assignment_Stephen_MacDonald/Assets/Scripts/Player/PlayerController.cs
+++ VGP123_Assignment_Stephen_MacDonald/Assets/Scripts/Player/PlayerController.cs
@@ -2,25 +2,79 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Rigidbody2D), typeof(SpriteRenderer), typeof(Animator))]
 public class PlayerController : MonoBehaviour
 {
     [Range(3f, 10)]
     public float speed = 5.5f;
-    
+    [Range(3f, 10)]
+    public float jumpForce = 3f;
+
     Rigidbody2D rb;
+    SpriteRenderer sr;
+    Animator anim;
+
+    [Range(0.01f, 0.1f)]
+    public float groundCheckRadius = 0.02f;
+    public LayerMask isGroundLayer;
     
+   
+    bool isGrounded = true;
+     Transform groundCheck;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+
+        GameObject newGameObject = new GameObject();
+        newGameObject.transform.SetParent(transform);
+        newGameObject.transform.localPosition = Vector3.zero;
+        newGameObject.name = "GroundCheck";
+        groundCheck = newGameObject.transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-       float hInput = Input.GetAxis("Horizontal");
+       CheckIsGrounded();
+
+        
+
+        
+        float hInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(hInput * speed, rb.velocity.y);
+
+
+
 
         rb.velocity = new Vector2(hInput * speed, rb.velocity.y);
+
+        if (Input.GetButtonDown("Jump") && isGrounded) 
+        { 
+            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+
+            isGrounded = false;
+        }
+
+        if (hInput != 0) sr.flipX = (hInput < 0);
+
+        //if (hInput > 0 && sr.flipX || hInput < 0 && !sr.flipX) sr.flipX = !sr.flipX;
+
+        anim.SetFloat("speed", Mathf.Abs(hInput));
+        anim.SetBool("isGrounded", isGrounded);
+        
+    }
+
+    void CheckIsGrounded() 
+    {
+        if (!isGrounded)
+        {
+            if (rb.velocity.y <= 0) isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, isGroundLayer);
+
+        }   
+        else isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, isGroundLayer);
     }
 }
